6d429c73ed93fab764ac72c74712b3fdd87562c1
Fatma Adel Youssef <fatmaeed2001@gmail.com>
2025-11-22 02:54:36 +0200

    fatma models

---
+++ Shatably/Models/CartItem.cs
@@ -0,0 +1,20 @@
+﻿namespace Shatably.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+
+        [ForeignKey("Cart")]
+        public int CartId { get; set; }
+
+        [ForeignKey("StoreProduct")]
+        public int StoreProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        // Relationships
+        public Cart Cart { get; set; }
+        public StoreProduct StoreProduct { get; set; }
+    }
+}
