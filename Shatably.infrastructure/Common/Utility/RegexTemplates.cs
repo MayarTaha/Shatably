480993a2f233d8cb5bb72f7ea21f032920521f00
randa sabra <randasabra27@gmail.com>
2025-11-26 00:47:03 +0200

    add classes

---
+++ Shatably.infrastructure/Common/Utility/RegexTemplates.cs
@@ -0,0 +1,8 @@
+﻿namespace Smart_Freelance_Infrastructure.Common.Utility;
+public static class RegexTemplates
+{
+    public const string Email = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+    public const string Password = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#_])[A-Za-z\d@$!%*?&#_]{8,}$";
+    public const string Otp = @"^\d{6}$";
+}
---
+++ Shatably.services/ModuleServiceDependencies.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shatably.services
+{
+    public static class ModuleServiceDependencies
+    {
+        public static IServiceCollection AddServiceDependencies(this IServiceCollection services)
+        {
+            // services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            return services;
+        }
+    }
+}
