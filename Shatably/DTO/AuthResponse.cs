e89d4cc0d61ad292b3fec462f24e4fc118a5c021
mayar <yorataha112@gmail.com>
2025-11-26 09:36:27 +0200

    Auth dto

---
+++ Shatably/DTO/AuthResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Shatably.DTO
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+}
