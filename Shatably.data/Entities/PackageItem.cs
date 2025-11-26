b03da98df81da9403d2bb9315abfb809e1e45f92
mayar <yorataha112@gmail.com>
2025-11-26 03:19:51 +0200

    add PackageItem model

---
+++ Shatably.data/Entities/PackageItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Shatably.Models
+{
+    public class PackageItem
+    {
+        public int PackageItemId { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+
+        public int PackageId { get; set; }
+        public Package Package { get; set; }
+
+        public int ProductId { get; set; }
+     
+        public Product Product { get; set; }
+    }
+}
